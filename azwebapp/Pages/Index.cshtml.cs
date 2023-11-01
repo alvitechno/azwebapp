e641e6950857cc98738e24daaf95bb17085d2dda
Sohaib Alvi <sohaibalvi.cloud@gmail.com>
2023-11-01 23:24:00 +0000

    Add project files.

---
+++ azwebapp/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using azwebapp.Models;
+using azwebapp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azwebapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products = productService.GetProducts();
+
+        }
+    }
+}
